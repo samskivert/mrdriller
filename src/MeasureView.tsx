0eecb93c8e3821327723352fd085bb4abdd848de
Michael Bayne <mdb@samskivert.com>
2025-10-18 16:26:28 -0700

    Various cleanup.

--- src/MeasureView.tsx
+++ src/MeasureView.tsx
@@ -1,6 +1,40 @@
 import * as React from "react"
-import { Measure } from "./model"
+import { Beat, Measure } from "./model"
 import { StrokeView } from "./StrokeView"
+
+const beatStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}
+
+const beatLabelStyle: React.CSSProperties = {
+  height: 20,
+  display: "flex",
+  alignItems: "center",
+  marginBottom: 4,
+}
+
+function BeatView({
+  beat,
+  forceLabel,
+  highlight,
+}: {
+  beat: Beat
+  forceLabel: boolean
+  highlight: boolean
+}) {
+  return (
+    <div style={beatStyle}>
+      {forceLabel && <div style={beatLabelStyle}>{beat.label ?? ""}</div>}
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {beat.strokes.map((stroke, strokeIndex) => (
+          <StrokeView key={strokeIndex} stroke={stroke} highlight={highlight} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export function MeasureView({
   measure,
@@ -10,49 +44,20 @@ export function MeasureView({
   highlightBeat?: number
 }) {
   const hasAnyLabels = measure.some((beat) => beat?.label)
-
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
-      {measure.map((beat, beatIndex) => {
-        if (!beat) {
-          return <div key={beatIndex} style={{ width: 36 }} />
-        }
-
-        const isBeatHighlighted = beatIndex === highlightBeat
-
-        return (
-          <div
+      {measure.map((beat, beatIndex) =>
+        beat ? (
+          <BeatView
             key={beatIndex}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            {hasAnyLabels && (
-              <div
-                style={{
-                  height: 20,
-                  display: "flex",
-                  alignItems: "center",
-                  marginBottom: 4,
-                }}
-              >
-                {beat.label && <div>{beat.label}</div>}
-              </div>
-            )}
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              {beat.strokes.map((stroke, strokeIndex) => (
-                <StrokeView
-                  key={strokeIndex}
-                  stroke={stroke}
-                  highlight={isBeatHighlighted}
-                />
-              ))}
-            </div>
-          </div>
-        )
-      })}
+            beat={beat}
+            forceLabel={hasAnyLabels}
+            highlight={beatIndex === highlightBeat}
+          />
+        ) : (
+          <div key={beatIndex} style={{ width: 36 }} />
+        ),
+      )}
     </div>
   )
 }
